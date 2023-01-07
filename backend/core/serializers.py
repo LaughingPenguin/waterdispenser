b5561d87046aeed4d75fc49a02755de16fc45104
Haoran Xu <sxu2469@gmail.com>
2023-01-07 16:04:25 -0500

    changed order of fields

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -9,8 +9,8 @@ class LocationsSerializer(serializers.ModelSerializer):
     name = CharField(source="title", required=True)
     email = EmailField(required=True)
     message = CharField(source="description", required=True)
-    longitude = CharField(source="description",required=True)
     latitude = CharField(source="description",required=True)
+    longitude = CharField(source="description",required=True)
     
     class Meta:
         model = models.Locations
@@ -18,6 +18,6 @@ class LocationsSerializer(serializers.ModelSerializer):
             'name',
             'email',
             'message',
-            'longitude',
-            'latitude'
+            'latitude',
+            'longitude'
         )
