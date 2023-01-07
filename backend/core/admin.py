b3b505f6c4f3c418c79ed3c17b0428aab6a94593
Haoran Xu <sxu2469@gmail.com>
2023-01-07 15:20:40 -0500

    added admin.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Locations
 
-# Register your models here.
+
+@admin.register(Locations)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'place', 'place', 'email')
